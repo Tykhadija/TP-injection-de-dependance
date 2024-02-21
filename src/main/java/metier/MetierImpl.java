3c58ef1b38a0afcf68079c3cbb211a77409a6fcb
Tykhadija <khadijatayoune@gmail.com>
2024-02-21 01:14:23 +0100

    first commit ijenction de dependance

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,14 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+
+
+    private IDao dao;
+
+    @Override
+    public double calcul() {
+        return 0;
+    }
+}
