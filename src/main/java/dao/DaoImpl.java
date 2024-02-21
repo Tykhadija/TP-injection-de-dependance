8440da6ee09d1d5f9f0028a96189d05c6eb952d9
Tykhadija <khadijatayoune@gmail.com>
2024-02-21 17:13:26 +0100

    second commit

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -1,9 +1,14 @@
 package dao;
 
+import org.springframework.stereotype.Component;
+
+@Component("dao")
 public class DaoImpl implements IDao {
 
     @Override
     public double getData() {
-        return 0;
+        System.out.println("version base de donnes");
+        double temp = 25;
+        return temp;
     }
 }
--- src/main/java/ext/DaoImpl2.java
+++ src/main/java/ext/DaoImpl2.java
@@ -1,12 +1,16 @@
 package ext;
 
 import dao.IDao;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository("dao2")
 public class DaoImpl2 implements IDao {
 
     @Override
     public double getData() {
-        System.out.println("version extention");
-        return 0;
+        System.out.println("version web service");
+        double t = 40;
+        return t;
     }
 }
